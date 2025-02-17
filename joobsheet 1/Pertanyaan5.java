5bf1247467bc1498dcbf6d754f8975bb9ffcc00a
geraldiakmal2380 <geraldiakmal612@gmail.com>
2025-02-17 07:26:47 +0700

    commit di pagi hari

---
+++ joobsheet 1/Pertanyaan5.java
@@ -0,0 +1,10 @@
+public class Pertanyaan5 {//array plat nomor dan input volume kubus
+    public static void main(String[] args) {
+        char[] kode = {'A','B','C','D','E','F','G','H','L','N','T'};
+        char[] kota = {'J','A','K','A','R','T','A'},
+                      {'B','A','N','T','E','N'},
+                      {'B','A','N','T','E','N'};
+
+
+    }
+}
