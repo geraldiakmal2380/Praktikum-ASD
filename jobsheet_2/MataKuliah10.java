0073531f32b036461fddbfffe70ffabd30681a75
geraldiakmal2380 <geraldiakmal612@gmail.com>
2025-02-28 17:54:25 +0700

    poiuyt

---
+++ jobsheet_2/MataKuliah10.java
@@ -0,0 +1,24 @@
+package jobsheet_2;
+
+public class MataKuliah10 {
+    String kodeMK,nama;
+    int jumlahjam,sks;
+    
+
+    void tampilkaninfromasi(){
+
+    }
+    
+    void ubahSKS(int sksbaru){
+
+    }
+
+    void tambajam(int jam){
+
+    }
+
+    void kurangijam(int jam){
+
+    }
+    
+}
---
+++ jobsheet_2/Matakuliahmain10.java
@@ -0,0 +1,9 @@
+package jobsheet_2;
+
+public class Matakuliahmain10 {
+    
+    public static void main(String[] args) {
+        
+    }
+
+}
